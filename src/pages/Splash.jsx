afd626af618e6abe28f20f0c04dec928d846409c
sjeda05-art <sjeda05@gmail.com>
2026-07-02 19:05:46 +0900

    Feat : 스플래쉬, 로그인 첫화면 기초틀 구현

---
+++ src/pages/Splash.jsx
@@ -0,0 +1,65 @@
+
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import BasicButton from '../components/BasicButton';
+
+
+const PageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 80px 0 60px;
+  gap: 60px; 
+`;
+
+const LogoBoxOut = styled.div`
+  width: 70%;
+  height: 50%;
+  background-color: #D9D9D9;
+  border-radius: 10px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoBoxInner = styled.div`
+  width: 80%;
+  height: 40%;
+  background-color: #FFFFFF;
+  border-radius: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StartButton = styled.div`
+  width: 70%;
+  height: 7%;
+  background-color: #D9D9D9;
+  border-radius: 12px;
+  padding: 14px 40px;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
+export default function Splash() {
+  const navigate = useNavigate();
+  return (
+    <PageWrapper>
+      <LogoBoxOut>
+        <LogoBoxInner>BeneFitU</LogoBoxInner>
+      </LogoBoxOut>
+      <StartButton onClick={() => navigate('/login-home') }>시작하기</StartButton>
+    </PageWrapper>
+  );
+}
